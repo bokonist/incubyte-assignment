fdfa2f3d2b93c52ac88e832f11556205558e0468
bokonist <jayrm1995@gmail.com>
2022-04-25 09:19:08 +0530

    syntax fix

--- testSuites.test.js
+++ testSuites.test.js
@@ -5,9 +5,9 @@ describe("TEST set 1 - simple comma separated numbers, empty strings", () => {
     expect(Add("")).toBe(0);
   });
   it("one single digit number", () => {
-    expect(Add("1").toBe(1));
+    expect(Add("1")).toBe(1);
   });
   it("one multi digit number ", () => {
-    expect(Add("12").toBe(12));
+    expect(Add("12")).toBe(12);
   });
 });
