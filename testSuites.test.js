dd2801f3c62e97446ff3e6c0646331b6f4c53ec1
bokonist <jayrm1995@gmail.com>
2022-04-25 09:25:06 +0530

    added tsts for multiple numbers, mutiple missing

--- testSuites.test.js
+++ testSuites.test.js
@@ -10,4 +10,23 @@ describe("TEST set 1 - simple comma separated numbers, empty strings", () => {
   it("one multi digit number ", () => {
     expect(Add("12")).toBe(12);
   });
+  it("two numbers, first one missing", () => {
+    expect(Add(",12")).toBe(12);
+  });
+
+  it("two numbers, second one missing", () => {
+    expect(Add("12,")).toBe(12);
+  });
+
+  it("two numbers", () => {
+    expect(Add("12,13")).toBe(25);
+  });
+
+  it("multiple numbers, many are missing", () => {
+    expect(Add("12,,,,,,,13")).toBe(25);
+  });
+
+  it("multiple numbers", () => {
+    expect(Add("1,2,3,4,50")).toBe(60);
+  });
 });
