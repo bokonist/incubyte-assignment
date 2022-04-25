b2f4ca0cfb02d49dc385a8fbc0284feed12239cf
bokonist <jayrm1995@gmail.com>
2022-04-25 09:06:10 +0530

    tests for single numbers

--- testSuites.test.js
+++ testSuites.test.js
@@ -4,4 +4,10 @@ describe("TEST set 1 - simple comma separated numbers, empty strings", () => {
   it("empty string", () => {
     expect(Add("")).toBe(0);
   });
+  it("one single digit number", () => {
+    expect(Add("1").toBe(1));
+  });
+  it("one multi digit number ", () => {
+    expect(Add("12").toBe(12));
+  });
 });
