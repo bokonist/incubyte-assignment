9d851b657f897fc3eb9001bb391cb8a0accf9f7c
bokonist <jayrm1995@gmail.com>
2022-04-25 09:42:08 +0530

    added tests for comma, newline delimit combos

--- testSuites.test.js
+++ testSuites.test.js
@@ -30,3 +30,32 @@ describe("TEST set 1 - simple comma separated numbers, empty strings", () => {
     expect(Add("1,2,3,4,50")).toBe(60);
   });
 });
+
+describe("TEST set 2 - comma and newline separated numbers", () => {
+  it("two numbers, newline separated, first one missing", () => {
+    expect(Add("\n12")).toBe(12);
+  });
+
+  it("two numbers, newline separated,  second one missing", () => {
+    expect(Add("12\n")).toBe(12);
+  });
+
+  it("two numbers, newline separated", () => {
+    expect(Add("12\n13")).toBe(25);
+  });
+
+  it("multiple numbers, newline separated, many are missing", () => {
+    expect(Add("12\n\n\n\n1\n\n13")).toBe(26);
+  });
+
+  it("multiple numbers, newline and comma separated, many are missing", () => {
+    expect(Add("12\n,\n,1\n,13,,,,\n\n\n2")).toBe(28);
+  });
+  it("multiple numbers, newline separated", () => {
+    expect(Add("1\n2\n3\n4\n50")).toBe(60);
+  });
+
+  it("multiple numbers, newline+comma separated", () => {
+    expect(Add("1\n2,3\n4,50")).toBe(60);
+  });
+});
