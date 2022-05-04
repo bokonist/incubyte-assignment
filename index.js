d6becd5f8bdf63e602e9ec723c14cb6ef184e793
bokonist <jayrm1995@gmail.com>
2022-05-04 14:23:52 +0530

    cleaner code + airbnb style lint checks

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const Add = (s) => {
-  if (s == "") return 0;
-
-  let delimiter;
-  if (/^[\/]{2}.\n/.test(s)) {
-    delimiter = "[\\n," + s[2] + "]+";
-    s = s.slice(4);
-    delimiter = RegExp(delimiter);
-    //return delimiter;
-  } else {
-    delimiter = /[\n,]+/;
+  if (s === "") {
+    return 0;
   }
-  let arr = s.split(delimiter);
-  let sum = arr
-    .map((el) => {
-      if (el == "") return 0;
-      else if (isNaN(el))
-        throw new Error(
-          "Invalid numbers passed: " + arr.filter((el) => isNaN(el)).join(",")
-        );
-      else if (el[0] == "-")
-        throw new Error(
-          "negatives not allowed: " +
-            arr.filter((el) => Number(el) < 0).join(",")
-        );
-      else return Number(el);
-    })
-    .reduce((el, acc) => {
-      acc += el;
-      return acc;
-    }, 0);
+
+  let delimiter = "";
+  let paramString = s;
+  let delimiterExp = /[\n,]+/;
+  let customDelimiter;
+  if (/^[/]{2}.\n/.test(s)) {
+    customDelimiter = s.charAt(2);
+    delimiter = `[\\n,${customDelimiter}]+`;
+    paramString = s.slice(4);
+    delimiterExp = RegExp(delimiter);
+  }
+
+  const arr = paramString.split(delimiterExp);
+
+  const negativeArr = [];
+  arr.forEach((element, index) => {
+    let num = Number(element);
+    if (Number.isNaN(num)) {
+      throw new Error(
+        `Invalid numbers passed: ${arr
+          .filter((el) => Number.isNaN(Number(el)))
+          .join(",")}`
+      );
+    }
+    if (num < 0) {
+      negativeArr.push(num);
+    } else if (num > 1000) {
+      num = 0;
+    }
+    arr[index] = num;
+  });
+  const isNegativeCountOdd = negativeArr.length % 2 === 1;
+  if (isNegativeCountOdd) {
+    throw new Error(`negatives not allowed: ${negativeArr.join(",")}`);
+  }
+  const sum = arr.reduce((el, tempSum) => tempSum + el, 0);
   return sum;
 };
-
 module.exports = Add;
